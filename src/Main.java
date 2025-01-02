a2fc8c8f53ffea3090f5d86a706e8131feced13b
Vitor Franca <vitorfranca089@gmail.com>
2025-01-02 18:11:07 -0300

    feat: add password generation based in charset selection

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,36 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        StringBuffer sb = new StringBuffer();
+        StringBuffer stringBuffer = new StringBuffer();
 
         int sizePass = scanner.nextInt();
 
-        final char[] ultimateChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%¨&*()-_=+,.;/".toCharArray();
+        int[] options = new int[4];
+
+        for(int i = 0; i < options.length; i++) options[i] = scanner.nextInt();
+
+        char[][] charset = {
+                "abcdefghijklmnopqrstuvwxyz".toCharArray(),
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray(),
+                "1234567890".toCharArray(),
+                "!@#$%¨&*()-_=+,.;/><?[]{}".toCharArray()
+        };
+
+        List<char[]> chosenCharset = new ArrayList<>();
+
+        for(int i = 0; i < options.length; i++) if(options[i] == 1) chosenCharset.add(charset[i]);
 
         for(int i = 0; i < sizePass; i++){
-            sb.append(ultimateChar[random.nextInt(ultimateChar.length)]);
+            char[] charsetRand = chosenCharset.get(random.nextInt(chosenCharset.size()));
+            int charRand = random.nextInt(charsetRand.length);
+            stringBuffer.append(charsetRand[charRand]);
         }
 
-        String password = sb.toString();
+        String password = stringBuffer.toString();
         System.out.println(password);
 
     }
