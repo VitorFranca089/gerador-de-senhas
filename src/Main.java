30f9b00ebee51b48a2bdfa834a9b6abac2910ba7
Vitor Franca <vitorfranca089@gmail.com>
2025-01-02 17:16:29 -0300

    feat: add password generation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+
+        int sizePass = scanner.nextInt();
+
+        final char[] ultimateChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%¨&*()-_=+,.;/".toCharArray();
+
+        for(int i = 0; i < sizePass; i++){
+            sb.append(ultimateChar[random.nextInt(ultimateChar.length)]);
+        }
+
+        String password = sb.toString();
+        System.out.println(password);
+
     }
 }
